a44230cacdaff9513a435a2b6a27817e1da4eaf1
Phob0s7 <villoz_sylvain@hotmail.com>
2020-10-27 07:03:32 +0100

    Ajout d'une barre de menu Test d'ajout d'un rectangle

--- M4-Puissance4/MainWindow.xaml.cs
+++ M4-Puissance4/MainWindow.xaml.cs
@@ -7,11 +7,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using Color = System.Drawing.Color;
 
 namespace M4_Puissance4
 {
@@ -23,6 +26,27 @@ namespace M4_Puissance4
         public MainWindow()
         {
             InitializeComponent();
+            this.boardColumns = new System.Drawing.Rectangle[7];
+        }
+
+        private System.Drawing.Rectangle[] boardColumns;
+
+        private void GameBoard_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+            e.Graphics.FillRectangle(blueBrush, 24, 24, 340, 300);
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i == 0)
+                    {
+                        this.boardColumns[j] = new System.Drawing.Rectangle(32 + 48 * j, 24, 32, 300);
+                    }
+                    e.Graphics.FillEllipse(blueBrush, 32 + 48 * j, 32 + 48 * i, 32, 32);
+                }
+            }
         }
     }
 }
